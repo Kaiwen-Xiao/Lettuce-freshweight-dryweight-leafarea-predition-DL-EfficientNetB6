5b8a96e6734b0614c2f00d75f49adff100c2e830
kaiwen xiao <126135993+Kaiwen-Xiao@users.noreply.github.com>
2023-11-29 00:04:10 -0500

    project.py

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+# !pip install -r requirements.txt
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -16,6 +18,10 @@ from efficientnet_pytorch import EfficientNet
 import os
 import multiprocessing
 from math import sqrt
+
+
+#### Be sure to chnage this to True when you are going to train the model
+train_enable = False
 
 # number of system CPU
 cpu_count = os.cpu_count()
@@ -88,7 +94,6 @@ class LettuceDataset(Dataset):
         center_x, center_y = rgb_image.shape[1] // 2, rgb_image.shape[0] // 2
         half_size = 540  # half of the new size 1080 / 2
         cropped_rgb_image = rgb_image[center_y - half_size:center_y + half_size, center_x - half_size:center_x + half_size]
-        # cropped_depth_image = depth_image[center_y - half_size:center_y + half_size, center_x - half_size:center_x + half_size]
 
         # Preprocess and stack depth with RGB
         if self.transform:
@@ -119,23 +124,8 @@ def train_model(model, criterion, optimizer, train_loader, val_loader, scheduler
             running_loss += loss.item() * inputs.size(0)
         
         epoch_loss = running_loss / len(train_loader.dataset)
-        
-        # model.eval()
-        # val_running_loss = 0.0
-        # with torch.no_grad():
-        #     for inputs, labels in val_loader:
-        #         inputs = inputs.to(device)
-        #         labels = labels.to(device).float()
-        #         outputs = model(inputs)
-        #         loss = criterion(outputs, labels)
-        #         val_running_loss += loss.item() * inputs.size(0)
-        
-        # val_epoch_loss = val_running_loss / len(val_loader.dataset)
-        # log_text = f'Epoch {epoch}/{epochs - 1}: Loss: {epoch_loss:.4f} - Val Loss: {val_epoch_loss:.4f}'
-        # print(log_text)
-        # with open("log.txt", "a") as file:
-        #     file.write(log_text+ "\n")
-        # scheduler.step(val_epoch_loss)
+
+        # Evaluation of the model
         model.eval()
         val_running_loss = 0.0
         actuals = []  # actual labels
@@ -161,12 +151,11 @@ def train_model(model, criterion, optimizer, train_loader, val_loader, scheduler
         rmse = sqrt(mean_squared_error(actuals, predictions))
         r2 = r2_score(actuals, predictions)
 
+        # Save log
         log_text = f'Epoch {epoch+1}/{epochs}: Loss: {epoch_loss:.4f} - Val Loss: {val_epoch_loss:.4f} - Val RMSE: {rmse:.4f} - Val R^2: {r2:.4f}'
         print(log_text)
 
-        # with open("log-b6.txt", "a") as file:
-        #     file.write(log_text+ "\n")
-        
+        # Save model if validation loss is improved
         if val_epoch_loss < best_loss:
             best_loss = val_epoch_loss
             torch.save(model.state_dict(), 'best_model_weights-b6.pth')
@@ -181,9 +170,10 @@ def train_model(model, criterion, optimizer, train_loader, val_loader, scheduler
                 file.write("\n" + "Validation loss did not improve"+ "\n")
         with open("log-b6.txt", "a") as file:
             file.write(log_text+ "\n")
-        # if patience_counter >= patience:
-        #     print('Early stopping triggered.')
-        #     break
+            
+        if patience_counter >= patience:
+            print('Early stopping triggered.')
+            break
     
     return model
 
@@ -203,15 +193,20 @@ def main():
     annotations = json.load(f)["Measurements"]
   annotations_list = list(annotations.values())
   train_annotations, val_annotations = train_test_split(annotations_list, test_size=0.1, random_state=42)
-  # print(val_annotations)
+  # print(val_annotations)#Uncomment this to check which images are used for validation
   train_set = LettuceDataset(json_file, rgb_dir, transform=rgb_transform)
   train_set.annotations = train_annotations
+
+  #This train_val_set is used to evaluate the training dataset, so it removed the image augmentation
+  train_val_set = LettuceDataset(json_file, rgb_dir, transform=validation_transform)
+  train_val_set.annotations = train_annotations
     
   val_set = LettuceDataset(json_file, rgb_dir,transform=validation_transform)
   val_set.annotations = val_annotations
 
   # Load data
   train_loader = DataLoader(train_set, batch_size=16, shuffle=True)
+  train_val_loader = DataLoader(train_set, batch_size=16, shuffle=False)
   val_loader = DataLoader(val_set, batch_size=16, shuffle=False)
   
   
@@ -232,24 +227,84 @@ def main():
   criterion = nn.MSELoss()
     
   # #Learning rate
-  Learing_rate = 5.0e-12
-  for i in range (10):
-      print(f'learing_rate: {Learing_rate}')
-      optimizer = optim.RMSprop(model.parameters(), Learing_rate)
-      op_text = f'RMS mode Start'
-      with open("log-b6.txt", "a") as file:
-        file.write(op_text+ "\n")
-      scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
-      model = train_model(model, criterion, optimizer, train_loader, val_loader, scheduler, epochs=25)   
-        
-      optimizer = optim.Adam(model.parameters(), Learing_rate)
-      op_text = f'Adam mode Start'
-      with open("log-b6.txt", "a") as file:
-        file.write(op_text+ "\n")
-      scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=20, verbose=True)
-      model = train_model(model, criterion, optimizer, train_loader, val_loader, scheduler, epochs=25)
-      Learing_rate /= 10
+  Learing_rate = 0.01
+
+  if train_enable:  
+      for i in range (10):
+          print(f'learing_rate: {Learing_rate}')
+          for j in range (2):             
+              optimizer = optim.RMSprop(model.parameters(), Learing_rate)
+              op_text = f'RMS mode Start'
+              with open("log-b6.txt", "a") as file:
+                file.write(op_text+ "\n")
+              scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=50, verbose=True)
+              model = train_model(model, criterion, optimizer, train_loader, val_loader, scheduler, epochs=25)   
+                
+              optimizer = optim.Adam(model.parameters(), Learing_rate)
+              op_text = f'Adam mode Start'
+              with open("log-b6.txt", "a") as file:
+                file.write(op_text+ "\n")
+              scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=50, verbose=True)
+              model = train_model(model, criterion, optimizer, train_loader, val_loader, scheduler, epochs=25)
+          
+          Learing_rate /= 10
+  else:
+    val_loss, (val_fresh_rmse, val_fresh_r2), (val_dry_rmse, val_dry_r2), (val_leaf_rmse, val_leaf_r2) = evaluate_model(model, val_loader, criterion, device)
+    print(f"Total Validation Loss: {val_loss}")
+    print(f"Fresh Weight - RMSE: {val_fresh_rmse}, R²: {val_fresh_r2}")
+    print(f"Dry Weight - RMSE: {val_dry_rmse}, R²: {val_dry_r2}")
+    print(f"Leaf Area - RMSE: {val_leaf_rmse}, R²: {val_leaf_r2}")
+      
+    val_loss, (val_fresh_rmse, val_fresh_r2), (val_dry_rmse, val_dry_r2), (val_leaf_rmse, val_leaf_r2) = evaluate_model(model, train_val_loader, criterion, device)
+    print(f"Total train Loss: {val_loss}")
+    print(f"train Fresh Weight - RMSE: {val_fresh_rmse}, R²: {val_fresh_r2}")
+    print(f"train Dry Weight - RMSE: {val_dry_rmse}, R²: {val_dry_r2}")
+    print(f"train Leaf Area - RMSE: {val_leaf_rmse}, R²: {val_leaf_r2}")
   return True
+
+def evaluate_model(model, val_loader, criterion, device):
+    model.eval()  # set the model to evaluation mode
+    val_running_loss = 0.0
+
+    # Store actuals and predictions for each output
+    actuals_fresh, predictions_fresh = [], []
+    actuals_dry, predictions_dry = [], []
+    actuals_leaf, predictions_leaf = [], []
+
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device).float()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_running_loss += loss.item() * inputs.size(0)
+
+            # Split actual and predicted values for each output
+            actuals_fresh.extend(labels.cpu()[:, 0].numpy())
+            predictions_fresh.extend(outputs.cpu()[:, 0].numpy())
+
+            actuals_dry.extend(labels.cpu()[:, 1].numpy())
+            predictions_dry.extend(outputs.cpu()[:, 1].numpy())
+
+            actuals_leaf.extend(labels.cpu()[:, 2].numpy())
+            predictions_leaf.extend(outputs.cpu()[:, 2].numpy())
+
+    # Total validation loss
+    val_loss = val_running_loss / len(val_loader.dataset)
+
+    # Calculate RMSE and R^2 for each output
+    fresh_rmse = sqrt(mean_squared_error(actuals_fresh, predictions_fresh))
+    fresh_r2 = r2_score(actuals_fresh, predictions_fresh)
+
+    dry_rmse = sqrt(mean_squared_error(actuals_dry, predictions_dry))
+    dry_r2 = r2_score(actuals_dry, predictions_dry)
+
+    leaf_rmse = sqrt(mean_squared_error(actuals_leaf, predictions_leaf))
+    leaf_r2 = r2_score(actuals_leaf, predictions_leaf)
+
+    return val_loss, (fresh_rmse, fresh_r2), (dry_rmse, dry_r2), (leaf_rmse, leaf_r2)
+
 
 if __name__ == "__main__":
   main()
